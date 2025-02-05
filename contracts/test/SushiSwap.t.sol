9e9d67e2c2539dfbc2860102d998ea6a2b322a98
cdrappi <c@seismic.systems>
2025-02-05 17:31:35 -0500

    add test

---
+++ contracts/test/SushiSwap.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import { PumpRand, Coin } from "../src/pump/PumpRand.sol";
+import { IPumpCoin } from "../src/pump/PumpCoin.sol";
+
+contract SushiSwapTest is Test {
+    function setUp() public {
+        
+    }
+}
