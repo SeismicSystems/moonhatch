87b0e6eac7c7118c08810b132309cd7c693a9cdb
mHaines331 <mhaines9219@gmail.com>
2025-02-11 11:11:03 -0800

    remove empty import

--- frontend/web/src/components/coin/coin-detail.tsx
+++ frontend/web/src/components/coin/coin-detail.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useShieldedWallet } from 'seismic-react'
-
 import { formatEther, parseEther } from 'viem'
 
 import { useContract } from '@/hooks/useContract'
