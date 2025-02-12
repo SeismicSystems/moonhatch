f0c1f86d3cb8ec6502cab485471584a37a1efccc
mHaines331 <mhaines9219@gmail.com>
2025-02-12 09:06:44 -0800

    add graduated to diesel table

--- backend/migrations/2025-02-11-204131_create_coins/up.sql
+++ backend/migrations/2025-02-11-204131_create_coins/up.sql
@@ -5,6 +5,7 @@ CREATE TABLE coins (
     supply NUMERIC NOT NULL,
     contract_address CHAR(42) NOT NULL,
     creator CHAR(42) NOT NULL,
+    graduated BOOLEAN DEFAULT FALSE,
     description TEXT,       -- To store the text description from the front end
     image_url TEXT,
     created_at TIMESTAMP DEFAULT NOW()
