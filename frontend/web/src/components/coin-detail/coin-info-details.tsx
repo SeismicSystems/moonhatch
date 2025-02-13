b47b7b917babb627b3f606785d409be4cdfa096b
mHaines331 <mhaines9219@gmail.com>
2025-02-13 15:25:56 -0800

    fetch description from database and display on UI

--- frontend/web/src/components/coin-detail/coin-info-details.tsx
+++ frontend/web/src/components/coin-detail/coin-info-details.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { Typography } from '@mui/material'
-
 interface CoinInfoDetailsProps {
   coin: {
     id: bigint
@@ -18,32 +16,37 @@ interface CoinInfoDetailsProps {
 
 const CoinInfoDetails: React.FC<CoinInfoDetailsProps> = ({ coin }) => {
   return (
-    <div className="flex justify-center">
-      <div className="left-column bg-[var(--darkBlue)] w-[300px] p-4 flex items-center justify-center space-x-4 rounded-2xl">
-        <div className="w-24 h-24">
-          <img
-            src={`https://seismic-public-assets.s3.us-east-1.amazonaws.com/pump/${coin.id.toString()}`}
-            alt="Coin Logo"
-            className="rounded-lg w-full h-full object-cover"
-          />
-        </div>
-        <div className="right-column flex flex-col text-left">
-          <div className="text-[18px] text-[var(--creamWhite)]">
-            {coin.name}
+    <>
+      <div className="flex justify-center">
+        <div className="left-column bg-[var(--darkBlue)] w-[300px] p-4 flex items-center justify-center space-x-4 rounded-2xl">
+          <div className="w-24 h-24">
+            <img
+              src={`https://seismic-public-assets.s3.us-east-1.amazonaws.com/pump/${coin.id.toString()}`}
+              alt="Coin Logo"
+              className="rounded-lg w-full h-full object-cover"
+            />
           </div>
-          <div className="text-[16px] text-[var(--creamWhite)] ">
-            $:{coin.symbol}
-          </div>
-          <div className=" text-[var(--lightBlue)] text-xs">
-            AUTHOR: {coin.creator?.toString().slice(0, 4)}...
-            {coin.creator?.toString().slice(-4) || 'N/A'}
-          </div>
-          <div className="text-[var(--lightBlue)] text-xs">
-            TIMESTAMP:{coin.createdAt.toString()}
+          <div className="right-column flex flex-col text-left">
+            <div className="text-[18px] text-[var(--creamWhite)]">
+              {coin.name}
+            </div>
+            <div className="text-[16px] text-[var(--creamWhite)] ">
+              $:{coin.symbol}
+            </div>
+            <div className=" text-[var(--lightBlue)] text-xs">
+              AUTHOR: {coin.creator?.toString().slice(0, 4)}...
+              {coin.creator?.toString().slice(-4) || 'N/A'}
+            </div>
+            <div className="text-[var(--lightBlue)] text-xs">
+              TIMESTAMP:{coin.createdAt.toString()}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="text-[var(--lightBlue)] flex flex-col text-xs">
+        <div className="coin-desc mb-2">"{coin.description}"</div>
+      </div>
+    </>
   )
 }
 
