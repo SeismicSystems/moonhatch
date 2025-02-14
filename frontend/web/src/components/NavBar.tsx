e4bcfe04a3ce703bf4655eed90b134461e6c359b
mHaines331 <mhaines9219@gmail.com>
2025-02-14 15:38:19 -0800

    create render prop for wallet display

--- frontend/web/src/components/NavBar.tsx
+++ frontend/web/src/components/NavBar.tsx
@@ -3,8 +3,11 @@ import { useNavigate } from 'react-router-dom'
 
 import { useAppState } from '@/hooks/useAppState'
 import HowItWorks from '@/pages/HowItWorks'
+import HelpIcon from '@mui/icons-material/Help'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
+
+import MyStyledConnectButton from './MyStyledConnectButton'
 
 const NavBar: React.FC = () => {
   const navigate = useNavigate()
@@ -14,20 +17,23 @@ const NavBar: React.FC = () => {
 
   return (
     <>
-      <nav className="w-full bg-white shadow-md py-4 px-8 flex justify-center space-x-8 text-lg font-medium text-gray-700">
+      <nav className="w-full bg-[var(--darkBlue)] shadow-md py-4 px-8 flex justify-center space-x-8 text-lg font-medium text-[var(--creamWhite)]">
         <button
           onClick={() => setShowHowItWorks(true)}
           className="hover:text-blue-600 transition"
         >
-          How it Works
+          <HelpIcon />
         </button>
         <button
           onClick={() => navigate('/create')}
-          className="hover:text-blue-600 transition"
+          className="hover:text-blue-600 text-[var(--creamWhite)] rounded-xl  transition border px-2 bg-[var(--midBlue)] whitespace-nowrap"
         >
-          New Coin
+          CREATE COIN
         </button>
-        <ConnectButton />
+        <div className="w-[10px] rounded-3xl">
+          <MyStyledConnectButton />
+          {/* <ConnectButton /> */}
+        </div>
       </nav>
       {showHowItWorks && (
         <HowItWorks
