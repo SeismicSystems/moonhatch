5a64a450dd6244097031ae4e122ea504c08b9da0
cdrappi <c@seismic.systems>
2025-02-17 11:56:26 -0500

    save changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,9 @@ use crate::db::create_coin;
 use crate::models::Coin;
 use crate::schema::coins::dsl::{coins, id, verified};
 
+use alloy_primitives::{hex, Address};
+use alloy_provider::network::TransactionBuilder;
+use alloy_rpc_types_eth::{TransactionInput, TransactionRequest};
 use axum::{
     extract::{Multipart, Path, State},
     http::{HeaderValue, Method, StatusCode},
@@ -10,17 +13,20 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use reqwest::Url;
 use db::NewCoin;
 use dotenv::dotenv;
+use std::str::FromStr;
 use std::{net::SocketAddr, sync::Arc};
 use tower_http::cors::{Any, CorsLayer};
-use alloy_provider::create_seismic_provider_without_wallet;
+use alloy_provider::{create_seismic_provider_without_wallet, SeismicPublicClient};
 
 // Declare our modules.
 mod db;
 mod db_pool;
 mod models;
 mod schema;
+mod abi;
 
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{config::Region, primitives::ByteStream, Client as S3Client};
@@ -30,6 +36,8 @@ use db_pool::{establish_pool, PgPool};
 pub struct AppState {
     pub s3_client: Arc<S3Client>,
     pub db_pool: PgPool,
+    pub seismic_client: Arc<SeismicPublicClient>,
+    pub contract_address: Address,
 }
 
 use diesel::prelude::*;
@@ -74,6 +82,9 @@ async fn verify_coin_handler(
                 .into_response()
         }
     };
+
+    let client = state.seismic_client;
+    // client.call();
 
     // Perform the update operation
     match diesel::update(coins.filter(id.eq(coin_id)))
@@ -224,11 +235,19 @@ async fn main() {
     let s3_client = S3Client::new(&aws_config);
     let shared_s3_client = Arc::new(s3_client);
 
+    // TODO: load from .env
+    // TODO: .env or load from file
+    let contract_address = Address::from_str("0x5FbDB2315678afecb367f032d93F642f64180aa3").unwrap();
+    let rpc_url = Url::from_str("http://127.0.0.1:8545").expect("invalid RPC_URL");
+    let seismic_client = create_seismic_provider_without_wallet(rpc_url);
+
     // Establish the database pool.
     let db_pool = establish_pool();
     let app_state = AppState {
         s3_client: shared_s3_client,
         db_pool,
+        seismic_client: Arc::new(seismic_client),
+        contract_address
     };
 
     // Set up CORS.
