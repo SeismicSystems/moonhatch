4fd3925cdbb09863db819bf352ea646959b468e2
mHaines <57149625+mHaines9219@users.noreply.github.com>
2025-02-17 15:17:59 -0800

    Responsive styling (#56)

--- frontend/web/src/pages/Coins.tsx
+++ frontend/web/src/pages/Coins.tsx
@@ -15,15 +15,15 @@ const Coins: React.FC<{ coins: Coin[] }> = ({ coins }) => {
   const currentCoins = coins.slice(indexOfFirstCoin, indexOfLastCoin)
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <div className="space-y-4">
+    <div className="max-w-7xl mx-auto  p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         {[...currentCoins].reverse().map((coin, index) => (
           <motion.div
             key={coin.id}
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: index * 0.2, duration: 1.0 }}
-            className="block" // ensures the motion.div is block-level so space-y-4 applies
+            className="block"
           >
             <CoinCard coin={coin} />
           </motion.div>
