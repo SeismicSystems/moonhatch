a5be3703d63edb341c1e46be361afd95bc4e076c
Christian Drappi <c@seismic.systems>
2025-02-17 10:31:26 -0500

    seed coin table with negative ids (#43)

--- backend/migrations/2025-02-11-222929_seed_coins/up.sql
+++ backend/migrations/2025-02-11-222929_seed_coins/up.sql
@@ -1,4 +1,4 @@
 INSERT INTO coins (id, name, symbol, supply, contract_address, creator, description, image_url, created_at)
 VALUES 
-(1, 'Bitcoin', 'BTC', 21000000, '0x0000000000000000000000000000000000000bbb', '0x0000000000000000000000000000000000000000', 'A decentralized digital currency.', 'https://seismic-public-assets.s3.us-east-1.amazonaws.com/seismic-logo-light.png', NOW()),
-(2, 'Ethereum', 'ETH', 115000000, '0x0000000000000000000000000000000000000eee', '0x0000000000000000000000000000000000000000', 'A programmable blockchain platform.', 'https://seismic-public-assets.s3.us-east-1.amazonaws.com/seismic-logo-light.png', NOW());
+(-1, 'Bitcoin', 'BTC', 21000000, '0x0000000000000000000000000000000000000bbb', '0x0000000000000000000000000000000000000000', 'A decentralized digital currency.', 'https://seismic-public-assets.s3.us-east-1.amazonaws.com/seismic-logo-light.png', NOW()),
+(-2, 'Ethereum', 'ETH', 115000000, '0x0000000000000000000000000000000000000eee', '0x0000000000000000000000000000000000000000', 'A programmable blockchain platform.', 'https://seismic-public-assets.s3.us-east-1.amazonaws.com/seismic-logo-light.png', NOW());
