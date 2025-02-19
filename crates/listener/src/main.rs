be02123eb8aa22a016b91da70e5d1c4c455ae83b
cdrappi <c@seismic.systems>
2025-02-18 19:06:37 -0500

    good

--- crates/listener/src/main.rs
+++ crates/listener/src/main.rs
@@ -50,16 +50,19 @@ async fn handle_log(log: Log, db_pool: &PgPool) -> Result<(), ListenerError> {
 }
 
 async fn handle_creation(log: Log<CoinCreated>, db_pool: &PgPool) -> Result<(), ListenerError> {
+    // TODO: move db models & schema here
     println!("Log: {:?} {:?}", log, db_pool);
     Ok(())
 }
 
 async fn handle_graduation(log: Log<CoinGraduated>, db_pool: &PgPool) -> Result<(), ListenerError> {
+    // TODO: move db models & schema here
     println!("Log: {:?} {:?}", log, db_pool);
     Ok(())
 }
 
 async fn handle_deploy(log: Log<DeployedToDex>, db_pool: &PgPool) -> Result<(), ListenerError> {
+    // TODO: move db models & schema here
     println!("Log: {:?} {:?}", log, db_pool);
     Ok(())
 }
