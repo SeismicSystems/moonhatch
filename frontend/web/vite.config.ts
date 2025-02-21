497e41bdb67f9cf1963a66b50b4fc2414ee27f8a
cdrappi <c@seismic.systems>
2025-02-21 16:21:56 -0500

    try automatic

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     target: 'esnext', // Use modern JavaScript features without transpiling down to older versions.
   },
   esbuild: {
-    jsx: 'transform',
+    jsx: 'automatic',
   },
 })
