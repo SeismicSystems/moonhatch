ca40a86a74092e8eea4f44a2d148771a2fd4cec5
mHaines331 <mhaines9219@gmail.com>
2025-02-21 09:05:52 -0500

    fix serach and filter field

--- frontend/web/src/components/home/search-and-filter.tsx
+++ frontend/web/src/components/home/search-and-filter.tsx
@@ -40,98 +40,104 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
 
   return (
     <div className="relative flex w-full justify-center items-center mt-6">
-      <input
-        type="text"
-        placeholder="SEARCH NAME/TICKER"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="w-64 text-xs lg:text-lg uppercase text-[var(--creamWhite)] h-12 lg:h-16 md:w-80 lg:w-96 p-2 rounded-md text-center"
-      />
+      {/* Wrapper to keep search and filter together */}
+      <div className="flex items-center">
+        {/* Search Input */}
+        <input
+          type="text"
+          placeholder="SEARCH NAME/TICKER"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-64 text-xs lg:text-lg uppercase text-[var(--creamWhite)] h-12 lg:h-16 md:w-80 lg:w-96 p-2 rounded-md text-center"
+        />
 
-      <div className="absolute right-20 lg:mr-24 xl:mr-72" ref={dropdownRef}>
-        <button
-          onClick={() => setIsDropdownOpen((prev) => !prev)}
-          className="px-4 py-2 h-12 lg:h-16 bg-[var(--midBlue)] text-[var(--creamWhite)] rounded-lg flex items-center justify-center"
-        >
-          <span className="hidden md:inline mr-1">FILTER</span>
-          <FilterListIcon className="md:hidden text-[var(--creamWhite)] text-lg" />
-        </button>
+        {/* Filter Button */}
+        <div className="ml-4 relative" ref={dropdownRef}>
+          <button
+            onClick={() => setIsDropdownOpen((prev) => !prev)}
+            className="px-4 py-2 h-12 lg:h-16 bg-[var(--midBlue)] text-[var(--creamWhite)] rounded-lg flex items-center justify-center"
+          >
+            <span className="hidden md:inline mr-1">FILTER</span>
+            <FilterListIcon className="md:hidden text-[var(--creamWhite)] text-lg" />
+          </button>
 
-        <AnimatePresence>
-          {isDropdownOpen && (
-            <motion.div
-              initial={{ opacity: 0, y: -10, scale: 0.95 }}
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              exit={{ opacity: 0, y: -10, scale: 0.95 }}
-              transition={{ duration: 0.2, ease: 'easeOut' }}
-              className="absolute right-0 mt-2 w-56 bg-[var(--lightBlue)] border border-gray-300 rounded-lg shadow-lg z-50 md:w-max"
-            >
-              <ul className="p-2 bg-[var(--midBlue)] text-[var(--creamWhite)] rounded-lg">
-                <li className="flex items-center gap-2 py-2 px-4 rounded-md font-bold text-[var(--creamWhite)]">
-                  SORT BY:
-                </li>
-                <li
-                  className="flex items-center gap-2 py-2 px-4 cursor-pointer rounded-md hover:bg-[var(--darkBlue)]"
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setFilters((prev) => ({
-                      ...prev,
-                      sortByCreatedAt: true,
-                    }))
-                  }}
-                >
-                  <div className="relative w-5 h-5 border-2 border-[var(--creamWhite)] rounded-md flex items-center justify-center">
-                    {filters.sortByCreatedAt && (
-                      <div className="w-3 h-3 bg-[var(--creamWhite)] rounded-sm"></div>
-                    )}
-                  </div>
-                  NEWEST
-                </li>
-                <li
-                  className="flex items-center gap-2 py-2 px-4 cursor-pointer rounded-md hover:bg-[var(--darkBlue)]"
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setFilters((prev) => ({
-                      ...prev,
-                      sortByCreatedAt: false,
-                    }))
-                  }}
-                >
-                  <div className="relative w-5 h-5 border-2 border-[var(--creamWhite)] rounded-md flex items-center justify-center">
-                    {!filters.sortByCreatedAt && (
-                      <div className="w-3 h-3 bg-[var(--creamWhite)] rounded-sm"></div>
-                    )}
-                  </div>
-                  OLDEST
-                </li>
-                <li className="border-t border-[var(--creamWhite)] my-2"></li>
-
-                {[
-                  { key: 'hasWebsite', label: 'WEBSITE' },
-                  { key: 'hasTelegram', label: 'TELEGRAM' },
-                  { key: 'hasTwitter', label: 'X' },
-                  { key: 'hasAllSocials', label: 'ALL SOCIALS' },
-                ].map(({ key, label }) => (
+          {/* Filter Dropdown */}
+          <AnimatePresence>
+            {isDropdownOpen && (
+              <motion.div
+                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                transition={{ duration: 0.2, ease: 'easeOut' }}
+                className="absolute right-0 mt-2 w-56 bg-[var(--lightBlue)] border border-gray-300 rounded-lg shadow-lg z-50 md:w-max"
+              >
+                <ul className="p-2 bg-[var(--midBlue)] text-[var(--creamWhite)] rounded-lg">
+                  <li className="flex items-center gap-2 py-2 px-4 rounded-md font-bold text-[var(--creamWhite)]">
+                    SORT BY:
+                  </li>
                   <li
-                    key={key}
                     className="flex items-center gap-2 py-2 px-4 cursor-pointer rounded-md hover:bg-[var(--darkBlue)]"
                     onClick={(e) => {
                       e.stopPropagation()
-                      setFilters((prev) => ({ ...prev, [key]: !prev[key] }))
+                      setFilters((prev) => ({
+                        ...prev,
+                        sortByCreatedAt: true,
+                      }))
                     }}
                   >
                     <div className="relative w-5 h-5 border-2 border-[var(--creamWhite)] rounded-md flex items-center justify-center">
-                      {filters[key] && (
+                      {filters.sortByCreatedAt && (
                         <div className="w-3 h-3 bg-[var(--creamWhite)] rounded-sm"></div>
                       )}
                     </div>
-                    {label}
+                    NEWEST
                   </li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
-        </AnimatePresence>
+                  <li
+                    className="flex items-center gap-2 py-2 px-4 cursor-pointer rounded-md hover:bg-[var(--darkBlue)]"
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      setFilters((prev) => ({
+                        ...prev,
+                        sortByCreatedAt: false,
+                      }))
+                    }}
+                  >
+                    <div className="relative w-5 h-5 border-2 border-[var(--creamWhite)] rounded-md flex items-center justify-center">
+                      {!filters.sortByCreatedAt && (
+                        <div className="w-3 h-3 bg-[var(--creamWhite)] rounded-sm"></div>
+                      )}
+                    </div>
+                    OLDEST
+                  </li>
+                  <li className="border-t border-[var(--creamWhite)] my-2"></li>
+
+                  {[
+                    { key: 'hasWebsite', label: 'WEBSITE' },
+                    { key: 'hasTelegram', label: 'TELEGRAM' },
+                    { key: 'hasTwitter', label: 'X' },
+                    { key: 'hasAllSocials', label: 'ALL SOCIALS' },
+                  ].map(({ key, label }) => (
+                    <li
+                      key={key}
+                      className="flex items-center gap-2 py-2 px-4 cursor-pointer rounded-md hover:bg-[var(--darkBlue)]"
+                      onClick={(e) => {
+                        e.stopPropagation()
+                        setFilters((prev) => ({ ...prev, [key]: !prev[key] }))
+                      }}
+                    >
+                      <div className="relative w-5 h-5 border-2 border-[var(--creamWhite)] rounded-md flex items-center justify-center">
+                        {filters[key] && (
+                          <div className="w-3 h-3 bg-[var(--creamWhite)] rounded-sm"></div>
+                        )}
+                      </div>
+                      {label}
+                    </li>
+                  ))}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   )
