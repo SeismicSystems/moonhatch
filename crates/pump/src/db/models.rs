eefa022d21a59c09e15d6f1b8c59b3aaad035581
cdrappi <c@seismic.systems>
2025-02-21 14:28:43 -0500

    OK

--- crates/pump/src/db/models.rs
+++ crates/pump/src/db/models.rs
@@ -8,28 +8,6 @@ use serde::{Deserialize, Serialize};
 
 use crate::{client::block::Block, db::schema, error::PumpError};
 
-/*
-CREATE TABLE coins (
-    id BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    symbol TEXT NOT NULL,
-    supply NUMERIC NOT NULL,
-    decimals INT NOT NULL,
-    contract_address CHAR(42) NOT NULL,
-    creator CHAR(42) NOT NULL,
-    graduated BOOLEAN DEFAULT FALSE NOT NULL,
-    verified BOOLEAN DEFAULT FALSE NOT NULL,
-    wei_in NUMERIC DEFAULT 0 NOT NULL,
-    description TEXT,
-    image_url TEXT,
-    website TEXT,
-    telegram TEXT,
-    twitter TEXT,
-    created_at TIMESTAMP DEFAULT NOW() NOT NULL
-);
-
-*/
-
 #[derive(Queryable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = schema::coins)]
 pub struct Coin {
