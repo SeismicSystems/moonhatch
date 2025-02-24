eaf09463737f8652547fdadf383e8871f957b655
Christian Drappi <c@seismic.systems>
2025-02-24 14:57:31 -0700

    Remove top-level await to support older build targets (#115)

--- frontend/web/src/hooks/useContract.ts
+++ frontend/web/src/hooks/useContract.ts
@@ -1,74 +1,24 @@
 import { useShieldedContract } from 'seismic-react'
-import { Hex } from 'viem'
 
-import type { ContractData } from '@/types/contract'
+import type { DeployedContract, ContractInterface } from '@/types/contract'
 
-export const CHAIN_ID = import.meta.env.VITE_CHAIN_ID
+import * as pumpJson from '@/abis/contracts/PumpRand.json' assert { type: 'json' }
+import * as dexJson from '@/abis/contracts/UniswapV2Router02.json' assert { type: 'json' }
+import * as coinJson from '@/abis/interfaces/IPumpCoin.json' assert { type: 'json' }
+import * as wethJson from '@/abis/contracts/WETH9.json' assert { type: 'json' }
 
-const loadContractData = async ({
-  name,
-  chainId = CHAIN_ID,
-  folder = 'contracts',
-}: {
-  name: string
-  chainId?: Hex | undefined
-  folder?: string
-}): Promise<ContractData> => {
-  // Check if VITE_CHAIN_ID is set
+const parseChainId = (): number => {
+  const chainId = '5124' // process.env.CHAIN_ID
   if (!chainId) {
-    throw new Error('loadContractData failed: chainId not set')
+    throw new Error('CHAIN_ID is not set')
   }
-
-  // Construct the file path (vite makes this load from /public directory)
-  const configPath = `/chains/${CHAIN_ID}/${folder}/${name}.json`
-  //modify this to send a string that is eiterh 'contracts' or 'interfaces' rn hardcoded to contracts
-  try {
-    const response = await fetch(configPath)
-    if (!response.ok) {
-      throw new Error(`Chain configuration file not found at: ${configPath}`)
-    }
-
-    return await response.json()
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      throw new Error(`Invalid JSON in chain configuration file: ${configPath}`)
-    }
-
-    // @ts-expect-error: error will have a message
-    throw new Error(`Error reading chain configuration: ${error.message}`)
-  }
+  return parseInt(chainId)
 }
 
-const pumpContractData = await loadContractData({ name: 'PumpRand' })
-const dexContractData = await loadContractData({ name: 'UniswapV2Router02' })
-const coinContractData = await loadContractData({
-  name: 'IPumpCoin',
-  folder: 'interfaces',
-})
+export const CHAIN_ID = parseChainId()
 
-export const WETH_CONTRACT_ADDRESS = await loadContractData({
-  name: 'WETH9',
-}).then((data) => data.address)
-export const PUMP_CONTRACT_ADDRESS = pumpContractData.address
-export const PUMP_CONTRACT_ABI = pumpContractData.abi
-export const usePumpContract = () =>
-  useShieldedContract({
-    abi: PUMP_CONTRACT_ABI,
-    address: PUMP_CONTRACT_ADDRESS,
-  })
+export const COIN_CONTRACT_ABI = (coinJson as ContractInterface).abi
 
-export const DEX_CONTRACT_ADDRESS = dexContractData.address
-export const DEX_CONTRACT_ABI = dexContractData.abi
-export const useDexContract = () =>
-  useShieldedContract({ abi: DEX_CONTRACT_ABI, address: DEX_CONTRACT_ADDRESS })
-
-export const COIN_CONTRACT_ABI = coinContractData.abi
-export const useCoinContract = (address: Hex) =>
-  useShieldedContract({
-    abi: COIN_CONTRACT_ABI,
-    address,
-  })
-
-// make export cont use-pumpcoin contract
-//user needs to put in param  unlike dex and pump
-//   useShieldedContract({ abi: DEX_CONTRACT_ABI, address: DEX_CONTRACT_ADDRESS })
+export const usePumpContract = () => useShieldedContract(pumpJson as DeployedContract)
+export const useDexContract = () => useShieldedContract(dexJson as DeployedContract)
+export const useWethContract = () => useShieldedContract(wethJson as DeployedContract)
