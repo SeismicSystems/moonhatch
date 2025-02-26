f8ec1b92951c5440c094827a9f7ebd6f2bd39238
mhaines9219 <mhaines9219@gmail.com>
2025-02-26 13:54:39 -0800

    Add useToastNotifications hook and implement for coin creation

---
+++ frontend/web/src/hooks/useToastNotifications.ts
@@ -0,0 +1,31 @@
+import { toast, ToastOptions } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const defaultOptions: ToastOptions = {
+  position: "top-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
+
+export const useToastNotifications = () => {
+  const notifySuccess = (message: string, options?: ToastOptions) => {
+    toast.success(message, { ...defaultOptions, ...options });
+  };
+
+  const notifyError = (message: string, options?: ToastOptions) => {
+    toast.error(message, { ...defaultOptions, ...options });
+  };
+
+  const notifyInfo = (message: string, options?: ToastOptions) => {
+    toast.info(message, { ...defaultOptions, ...options });
+  };
+
+  const notifyWarning = (message: string, options?: ToastOptions) => {
+    toast.warn(message, { ...defaultOptions, ...options });
+  };
+
+  return { notifySuccess, notifyError, notifyInfo, notifyWarning };
+};
