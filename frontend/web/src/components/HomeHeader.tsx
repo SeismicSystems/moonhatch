8c413e987802fbf44a48ce0e2dfc57085662043e
mhaines9219 <mhaines9219@gmail.com>
2025-02-26 09:19:34 -0800

    extract coin create button to component

--- frontend/web/src/components/HomeHeader.tsx
+++ frontend/web/src/components/HomeHeader.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { useAppState } from '@/hooks/useAppState'
-import HowItWorks from '@/pages/HowItWorks'
-import HelpIcon from '@mui/icons-material/Help'
 import LockIcon from '@mui/icons-material/Lock'
 import SchoolIcon from '@mui/icons-material/School'
-
 import KingOfTheHillSection from './home/king-of-the-hill'
+import CreateCoinButton from './home/create-coin-button'
 
 interface Coin {
   id: string
@@ -19,37 +13,11 @@ interface HomeHeaderProps {
   coins: Coin[]
 }
 export default function HomeHeader({ coins }: HomeHeaderProps) {
-  const navigate = useNavigate()
-  const { acceptedTerms } = useAppState()
-  const [showHowItWorks, setShowHowItWorks] = useState(!acceptedTerms())
   return (
     <>
       <div className="flex flex-col   items-center w-full px-4">
-        <button
-          onClick={() => setShowHowItWorks(true)}
-          className="text-orange-300 md:mr-72 lg:mr-96  -mb-15  mr-48  z-50  hover:text-blue-600 transition justify-self-start "
-        >
-          <HelpIcon
-            sx={{
-              fontSize: { xs: '20px', sm: '30px', md: '34px' },
-            }}
-          />
-        </button>
-        <button
-          onClick={() => navigate('/create')}
-          className=" w-[200px] md:w-[300px]  mt-12 lg:w-[400px] max-w-3xl lg:mt-12 text-[var(--creamWhite)] rounded-xl transition border px-2 text-xl md:text-2xl lg:text-3xl whitespace-nowrap h-16 md:h-20 lg:h-28 justify-self-center bg-[#161b33] hover:bg-green-500 hover:text-[#0d0c1d] "
-        >
-          CREATE COIN
-        </button>
+        <CreateCoinButton/>
         <KingOfTheHillSection coins={coins} />
-        {/* Right section: Empty placeholder to balance the grid */}
-
-        {showHowItWorks && (
-          <HowItWorks
-            isOpen={showHowItWorks}
-            onClose={() => setShowHowItWorks(false)}
-          />
-        )}
       </div>
       <div className="justify-self-end  lg:mt-2 flex-col flex text-[var(--creamWhite)]"></div>
       <div className="flex grad-icons-container gap-2 justify-center ">
