4bc2f430308674714964652f7f518c14b2e3801e
mhaines9219 <mhaines9219@gmail.com>
2025-02-26 08:56:07 -0800

    extract coin description into component

--- frontend/web/src/components/coin-detail/coin-info-details.tsx
+++ frontend/web/src/components/coin-detail/coin-info-details.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import { formatRelativeTime } from '@/util'
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 import CoinImageComponent from './coin-image-component'
+import CoinDescriptionContainer from './coin-desc-container'
 
 export const FALLBACK_COIN_IMAGE_URL =
   'https://seismic-public-assets.s3.us-east-1.amazonaws.com/seismic-logo-light.png'
@@ -31,29 +31,7 @@ const CoinInfoDetails: React.FC<CoinInfoDetailsProps> = ({ coin }) => {
   return (
     <>
       <CoinImageComponent coin={coin}  />
-      <Box
-      sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      color: 'var(--lightBlue)',
-      fontSize: '0.75rem',
-      '& .coin-desc': {
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-      marginBottom: 2,
-      fontSize: { xs: '1rem', sm: '1rem', md: '1.2rem', lg: '1.4rem' },
-      },
-      }}
-      >
-      <div className="coin-desc mt-2 flex flex-col items-center justify-center">
-      <div className="flex text-xs xl:text-lg text-orange-300">
-      DESCRIPTION:{' '}
-      </div>
-      <div className="lg:text-lg overflow-hidden text-ellipsis">
-      "{coin.description || 'creator did not provide description'}"
-      </div>
-      </div>
-      </Box>
+    <CoinDescriptionContainer coin={coin} />  
     </>
   )
 }
