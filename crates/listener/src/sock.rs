ea7430e17395722f69c1c9bb30d827ab9b392fd0
Christian Drappi <christiandrappi@gmail.com>
2025-02-27 14:50:07 -0700

    OK

--- crates/listener/src/sock.rs
+++ crates/listener/src/sock.rs
@@ -2,19 +2,10 @@ use std::{io::prelude::*, os::unix::net::UnixStream};
 
 use alloy_primitives::Address;
 use bigdecimal::BigDecimal;
-use pump::{db::models::Coin, error::ListenerError, SOCKET_PATH};
-use serde::{Deserialize, Serialize};
+use pump::{db::models::Coin, error::ListenerError, ListenerUpdate, SOCKET_PATH};
 
 pub struct SockWriter {
     stream: UnixStream,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) enum WsUpdate {
-    VerifiedCoin(Coin),
-    WeiInUpdated { coin_id: i64, total_wei_in: BigDecimal },
-    GraduatedCoin { coin_id: i64 },
-    DeployedToDex { coin_id: i64, deployed_pool: String },
 }
 
 impl SockWriter {
@@ -23,14 +14,14 @@ impl SockWriter {
         Ok(Self { stream })
     }
 
-    fn write(&mut self, message: &WsUpdate) -> Result<(), ListenerError> {
+    fn write(&mut self, message: &ListenerUpdate) -> Result<(), ListenerError> {
         let message = serde_json::to_string(message)?;
         self.stream.write_all(message.as_bytes())?;
         Ok(())
     }
 
     pub(crate) fn write_verified_coin(&mut self, coin: Coin) -> Result<(), ListenerError> {
-        self.write(&WsUpdate::VerifiedCoin(coin))
+        self.write(&ListenerUpdate::VerifiedCoin(coin))
     }
 
     pub(crate) fn write_wei_in_updated(
@@ -38,11 +29,11 @@ impl SockWriter {
         coin_id: i64,
         total_wei_in: BigDecimal,
     ) -> Result<(), ListenerError> {
-        self.write(&WsUpdate::WeiInUpdated { coin_id, total_wei_in })
+        self.write(&ListenerUpdate::WeiInUpdated { coin_id, total_wei_in })
     }
 
     pub(crate) fn write_graduated_coin(&mut self, coin_id: i64) -> Result<(), ListenerError> {
-        self.write(&WsUpdate::GraduatedCoin { coin_id })
+        self.write(&ListenerUpdate::GraduatedCoin { coin_id })
     }
 
     pub(crate) fn write_deployed_to_dex(
@@ -50,6 +41,9 @@ impl SockWriter {
         coin_id: i64,
         deployed_pool: Address,
     ) -> Result<(), ListenerError> {
-        self.write(&WsUpdate::DeployedToDex { coin_id, deployed_pool: deployed_pool.to_string() })
+        self.write(&ListenerUpdate::DeployedToDex {
+            coin_id,
+            deployed_pool: deployed_pool.to_string(),
+        })
     }
 }
