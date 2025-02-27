e6ac404d08749aee9de048fc6a2630d7bfc8609c
mhaines9219 <mhaines9219@gmail.com>
2025-02-27 10:21:52 -0800

    remove typo from reacthookform import

--- frontend/web/src/components/create/coin-form.tsx
+++ frontend/web/src/components/create/coin-form.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { set, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { useShieldedWallet } from 'seismic-react'
 import { hexToNumber } from 'viem'
