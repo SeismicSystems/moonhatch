010c612de73ddf9e863efd159bbd305776ee1c08
cdrappi <c@seismic.systems>
2025-03-04 14:49:39 -0500

    ok

--- crates/pump/src/client/ws.rs
+++ crates/pump/src/client/ws.rs
@@ -2,28 +2,28 @@ use crate::{
     client::contract_address::ContractAddresses, contract::pair::UniswapV2Pair, error::PumpError,
 };
 use alloy_primitives::{Address, FixedBytes};
-use alloy_provider::{Provider, ProviderBuilder, RootProvider, SeismicUnsignedWsProvider};
+use alloy_provider::{Provider, SeismicUnsignedWsProvider};
 use alloy_pubsub::SubscriptionStream;
 use alloy_rpc_types_eth::{Filter, Header, Log};
 use alloy_sol_types::SolEvent;
-use alloy_transport::{BoxTransport, TransportError};
+use alloy_transport::TransportError;
 
 #[derive(Debug, Clone)]
 pub struct PumpWsClient {
-    pub ws: RootProvider<BoxTransport>,
+    pub ws: SeismicUnsignedWsProvider,
     ca: ContractAddresses,
 }
 
 impl PumpWsClient {
     pub async fn new(chain_id: u64) -> Result<PumpWsClient, TransportError> {
         let ws_url = std::env::var("WS_RPC_URL").expect("Missing WS_RPC_URL in .env");
-        let ws = ProviderBuilder::new().on_builtin(&ws_url).await?;
+        let ws = SeismicUnsignedWsProvider::new(ws_url).await?;
         Ok(PumpWsClient { ws, ca: ContractAddresses::new(chain_id) })
     }
 
     pub async fn pump_logs(&self) -> Result<SubscriptionStream<Log>, PumpError> {
         let pump_filter = Filter::new().address(self.ca.pump);
-        let sub = self.ws.subscribe_logs(&pump_filter).await?;
+        let sub = self.ws.inner().subscribe_logs(&pump_filter).await?;
         Ok(sub.into_stream())
     }
 
@@ -35,17 +35,17 @@ impl PumpWsClient {
             UniswapV2Pair::Swap::SIGNATURE_HASH,
             UniswapV2Pair::Sync::SIGNATURE_HASH,
         ]);
-        let sub = self.ws.subscribe_logs(&filter).await?;
+        let sub = self.ws.inner().subscribe_logs(&filter).await?;
         Ok(sub.into_stream())
     }
 
     pub async fn blocks(&self) -> Result<SubscriptionStream<Header>, PumpError> {
-        let sub = self.ws.subscribe_blocks().await?;
+        let sub = self.ws.inner().subscribe_blocks().await?;
         Ok(sub.into_stream())
     }
 
     pub async fn unsubscribe(&self, sub_id: FixedBytes<32>) -> Result<(), PumpError> {
-        self.ws.unsubscribe(sub_id)?;
+        self.ws.inner().unsubscribe(sub_id)?;
         Ok(())
     }
 }
