0a28c58a9582b04301ec261ce1f185050977337c
cdrappi <c@seismic.systems>
2025-04-03 10:43:25 -0400

    backend WS: when client sends close message, close the connection

--- crates/server/src/ws.rs
+++ crates/server/src/ws.rs
@@ -31,15 +31,29 @@ async fn handle_socket(socket: WebSocket, state: WsState) {
 
     let mut broadcast_rx = state.tx.subscribe();
 
+    // Create a simple signal channel for clean shutdown
+    let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel();
+
     let client_id_clone = client_id.clone();
     let forwarding_task = tokio::spawn(async move {
-        while let Ok(msg) = broadcast_rx.recv().await {
-            log::debug!("Forwarding message to client {}: {}", client_id_clone, msg);
-            match sender.send(Message::Text(msg)).await {
-                Ok(_) => {}
-                Err(e) => {
-                    log::error!("Failed to send message to client {}: {:?}", client_id_clone, e);
+        loop {
+            tokio::select! {
+                // Exit loop when shutdown signal received
+                _ = &mut shutdown_rx => {
+                    log::debug!("Shutdown signal received for client {}", client_id_clone);
                     break;
+                },
+                msg = broadcast_rx.recv() => {
+                    if let Ok(msg) = msg {
+                        log::debug!("Forwarding message to client {}: {}", client_id_clone, msg);
+                        if let Err(e) = sender.send(Message::Text(msg)).await {
+                            log::error!("Failed to send message to client {}: {:?}", client_id_clone, e);
+                            break;
+                        }
+                    } else {
+                        // Broadcast channel closed
+                        break;
+                    }
                 }
             }
         }
@@ -47,10 +61,23 @@ async fn handle_socket(socket: WebSocket, state: WsState) {
 
     let client_id_clone = client_id.clone();
     let client_task = tokio::spawn(async move {
-        while let Some(Ok(Message::Text(text))) = receiver.next().await {
-            log::debug!("Received message from client {}: {}", client_id_clone, text);
-            // NOTE: handle client-to-server messages here if needed
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Text(text) => {
+                    // NOTE: handle client-to-server messages here if needed
+                    log::debug!("Received message from client {}: {}", client_id_clone, text);
+                }
+                Message::Close(_) => {
+                    log::debug!("Client {} sent close frame", client_id_clone);
+                    break;
+                }
+                _ => {}
+            }
         }
+
+        // Signal the forwarding task to stop when client disconnects
+        let _ = shutdown_tx.send(());
+        log::debug!("Client {} disconnected in receiver task", client_id_clone);
     });
 
     tokio::select! {
