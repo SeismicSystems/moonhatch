8226f0633c99e74071d19e00088a49d60e2a0f34
cdrappi <c@seismic.systems>
2025-04-04 09:56:01 -0400

    show an even number of coins per row

--- packages/web/src/pages/Coins.tsx
+++ packages/web/src/pages/Coins.tsx
@@ -5,18 +5,19 @@ import CoinCard from '@/components/coin/coin-card'
 import Pagination from '@/components/coin/pagination'
 import type { Coin } from '@/types/coin'
 
+const COINS_PER_PAGE = 24
+
 const Coins: React.FC<{ coins: Coin[] }> = ({ coins }) => {
   const [currentPage, setCurrentPage] = useState(1)
-  const itemsPerPage = 10
 
-  const indexOfLastCoin = currentPage * itemsPerPage
-  const indexOfFirstCoin = indexOfLastCoin - itemsPerPage
+  const indexOfLastCoin = currentPage * COINS_PER_PAGE
+  const indexOfFirstCoin = indexOfLastCoin - COINS_PER_PAGE
   const currentCoins = coins.slice(indexOfFirstCoin, indexOfLastCoin)
 
   return (
     <div className="max-w-7xl mx-auto p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        {[...currentCoins].reverse().map((coin, index) => (
+        {[...currentCoins].map((coin, index) => (
           <motion.div
             key={coin.id}
             initial={{ rotateY: 90, opacity: 0 }}
@@ -54,7 +55,7 @@ const Coins: React.FC<{ coins: Coin[] }> = ({ coins }) => {
 
       <Pagination
         totalItems={coins.length}
-        itemsPerPage={itemsPerPage}
+        itemsPerPage={COINS_PER_PAGE}
         currentPage={currentPage}
         onPageChange={setCurrentPage}
       />
