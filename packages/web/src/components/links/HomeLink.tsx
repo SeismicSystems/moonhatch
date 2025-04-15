29d398a331db154225800106aa991e407df3126e
mHaines <mhaines9219@gmail.com>
2025-04-15 13:32:41 -0700

    fix weird HOF navbar btn issue

--- packages/web/src/components/links/HomeLink.tsx
+++ packages/web/src/components/links/HomeLink.tsx
@@ -6,10 +6,23 @@ import HomeIcon from '@mui/icons-material/Home'
 const HomeLink = ({ style }: { style?: React.CSSProperties }) => {
   const navigate = useNavigate()
   return (
-    <IconLink onClick={() => navigate('/')} text="Home" style={style}>
+    <IconLink
+      onClick={() => navigate('/')}
+      text="Home"
+      style={{
+        ...style,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        position: 'relative',
+        zIndex: 1,
+      }}
+    >
       <HomeIcon
         sx={{
           fontSize: { xs: '20px', sm: '30px', md: '34px' },
+          width: '100%',
         }}
       />
     </IconLink>
