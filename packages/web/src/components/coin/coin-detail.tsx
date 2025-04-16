aecca42be169c21905f554fc7fc0125711745c4a
mHaines <mhaines9219@gmail.com>
2025-04-16 13:31:08 -0700

    more ui-fixes

--- packages/web/src/components/coin/coin-detail.tsx
+++ packages/web/src/components/coin/coin-detail.tsx
@@ -20,8 +20,17 @@ const CoinDetailGraph: React.FC<{ coin: Coin }> = ({ coin }) => {
   return (
     <>
       {coin.graduated && coin.deployedPool ? (
-        <Box sx={{ display: 'flex' }}>
-          <div className="justify-center items-center">
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 'auto',
+            border: { lg: '2px solid var(--creamWhite)', xs: 'none' },
+            borderRadius: '10px',
+          }}
+        >
+          <div className="-mt-32 mb-8 md:mb-0 md:-mt-0">
             <Candles pool={`${coin.deployedPool}`} />
           </div>
         </Box>
@@ -32,8 +41,8 @@ const CoinDetailGraph: React.FC<{ coin: Coin }> = ({ coin }) => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            height: '350px',
-            width: '350px',
+            height: '20rem',
+
             marginRight: { xs: 0, sm: 0, md: 0, lg: 0, xl: '200%' },
           }}
         >
@@ -66,50 +75,65 @@ const CoinDetailContent: React.FC<{ coinId: string }> = ({ coinId }) => {
 
   return (
     <>
-      <div className="">
+      <div className="flex flex-col justify-center items-center">
         <NavBar />
-      </div>
-      <div className="page-container flex w-full justify-center items-center = flex-col lg:flex-row gap-12 xl:gap-24 pb-24 max-w-screen lg:px-12 2xl:px-24 lg:py-8">
         <Box
           sx={{
-            width: {
-              xs: 'auto',
-              sm: 'auto',
-              md: 'auto',
-              lg: 'auto',
-              xl: 'auto',
-            },
-            maxWidth: '100%',
-            mx: 'auto',
-            marginTop: 4,
-            p: 4,
-
-            bgcolor: 'var(--darkBlue)',
-            borderRadius: '24px',
-            overflow: 'hidden',
-            mb: { xs: 0, sm: 4, md: 0 },
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: { xs: 'column', lg: 'row' },
+            gap: '3rem',
+            maxWidth: 'screen-xl',
+            py: { lg: 2 },
           }}
         >
-          <CoinInfoDetails coin={coin} />
-          <TradeSection coin={coin} />
-          {(coin.twitter || coin.telegram || coin.website) && (
-            <div className="coin-socials-container -mt-12 lg:mt-20 -mb-4">
-              <CoinSocials
-                coin={{
-                  ...coin,
-                  id: coin.id,
-                  twitter: coin.twitter || '',
-                  telegram: coin.telegram || '',
-                  website: coin.website || '',
-                }}
-              />
-            </div>
-          )}
-        </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: {
+                xs: '90%',
+                sm: '90%',
+                md: '80%',
+                lg: '70%',
+                xl: '50%',
+              },
+              maxWidth: '100%',
+              marginTop: 4,
+              marginLeft: { xs: 0, lg: 4, xl: 16 },
 
-        <div className="status-icon-container flex justify-center mx-auto items-center">
-          <CoinDetailGraph coin={coin} />
-        </div>
+              p: 4,
+              border: '2px solid var(--creamWhite)',
+              bgcolor: 'var(--darkBlue)',
+              borderRadius: '24px',
+
+              mb: { xs: 0, sm: 4, md: 0 },
+            }}
+          >
+            <CoinInfoDetails coin={coin} />
+            <TradeSection coin={coin} />
+            {(coin.twitter || coin.telegram || coin.website) && (
+              <div className="coin-socials-container  -mt-12 lg:mt-20 -mb-4">
+                <CoinSocials
+                  coin={{
+                    ...coin,
+                    id: coin.id,
+                    twitter: coin.twitter || '',
+                    telegram: coin.telegram || '',
+                    website: coin.website || '',
+                  }}
+                />
+              </div>
+            )}
+          </Box>
+          <div className="status-icon-container lg:mr-8 xl:mr-32 flex justify-center mx-auto items-center ">
+            <CoinDetailGraph coin={coin} />
+          </div>
+        </Box>
       </div>
     </>
   )
@@ -128,7 +152,7 @@ const CoinDetail: React.FC = () => {
   }, [coinId, navigate, notifyError])
 
   if (!coinId) {
-    return <div>No coin ID</div>
+    return <div className="text-red-500">No coin ID</div>
   }
   return <CoinDetailContent coinId={coinId} />
 }
